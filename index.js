34af58b552c44c0c636f7d5e7aa28c037468ec1b
pm123baute <pedroemiliobaute@gmail.com>
2023-09-11 18:12:54 -0400

    bd

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function Reset(){
     formulario.Reset()
 }
+
+function Enviar(){
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'casa'
+      });
+      
+}
