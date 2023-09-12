8b8431be9a1dabbdd789d9740caa64b535082733
pm123baute <pedroemiliobaute@gmail.com>
2023-09-11 20:17:07 -0400

    cambios de pedro

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function Reset(){
-    formulario.Reset()
+import mysql from 'mysql';
+var sql = "SELECT `ID`,`NombreCompleto` From registros WHERE User=? AND Contraseña=? AND Email=?"
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'casa'
+});
+
+export function Enviar() {
+    
+    connection.connect()
+
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error;        
+        console.log(results);
+        connection.end();
+      });
+    
 }
 
-function Enviar(){
-    const mysql = require('mysql');
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'casa'
-      });
-      
+
+function Reset(){
+    form.Reset();
 }
